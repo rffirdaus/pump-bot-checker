ec2f57c268ed87f8d713d5f8fbed8a0475b37f47
unknown <ATN-3@LPT-ATN0720-49.astratel.co.id>
2025-04-14 23:02:54 +0700

    add user

--- api/pump-checker.js
+++ api/pump-checker.js
@@ -20,7 +20,7 @@ module.exports = async (req, res) => {
       const changePercent = ((lastPrice - prevPrice) / prevPrice) * 100;
 
       if (changePercent >= 1) {
-        const msg = `🚀 *PUMP ALERT!*\n\n🪙 Koin: *${symbol.toUpperCase()}*\n💰 Harga Terbaru: *${lastPrice}*\n Harga Sebelumnya: *${prevPrice}* \n 📈 Naik: *${changePercent.toFixed(2)}%*`;
+        const msg = `🚀 *PUMP ALERT!*\n\n🪙 Koin: *${symbol.toUpperCase()}*\n💰 Harga Terbaru: *${lastPrice}*\n 💰 Harga Sebelumnya: *${prevPrice}* \n 📈 Naik: *${changePercent.toFixed(2)}%*`;
 
         for (const chatId of CHAT_IDS) {
           await bot.sendMessage(chatId, msg, { parse_mode: "Markdown" });
