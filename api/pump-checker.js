97e930bebb8b921532850de3c431c4edb5237570
unknown <ATN-3@LPT-ATN0720-49.astratel.co.id>
2025-04-14 23:01:34 +0700

    add user

--- api/pump-checker.js
+++ api/pump-checker.js
@@ -19,7 +19,7 @@ module.exports = async (req, res) => {
       const prevPrice = lastPrices[symbol] || lastPrice;
       const changePercent = ((lastPrice - prevPrice) / prevPrice) * 100;
 
-      if (changePercent >= 10) {
+      if (changePercent >= 1) {
         const msg = `🚀 *PUMP ALERT!*\n\n🪙 Koin: *${symbol.toUpperCase()}*\n💰 Harga Terbaru: *${lastPrice}*\n Harga Sebelumnya: *${prevPrice}* \n 📈 Naik: *${changePercent.toFixed(2)}%*`;
 
         for (const chatId of CHAT_IDS) {
