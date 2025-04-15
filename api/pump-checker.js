fc4e34f8244512d8c65f71d1fc04073d3b4a0149
unknown <ATN-3@LPT-ATN0720-49.astratel.co.id>
2025-04-15 13:40:25 +0700

    add user

--- api/pump-checker.js
+++ api/pump-checker.js
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
       const coinName = symbol.replace('idr', '').toUpperCase() + '/IDR';
 
       // 🚀 Pump Alert
-      if (changePercent >= 10) {
+      if (changePercent >= 8) {
         let pumpMsg = `🚀 *PUMP TERDETEKSI!*\n\n🪙 Koin: *${coinName}*\n💰 Harga Terbaru: *${lastPrice}*\n💰 Harga Sebelumnya: *${prevPrice}*\n📈 Kenaikan: *${changePercent.toFixed(2)}%*`;
 
         // 🔍 Analisis Spread Harga (hanya jika pump terdeteksi)
